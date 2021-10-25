3c215c65743f8ccf622b775a4ff5b34bbe5625ca
skunin1 <92861957+skunin1@users.noreply.github.com>
2021-10-25 13:33:22 +0530

    Delete ff4JDemoServer directory

--- ff4JDemoServer/ff4JDemoServer/src/main/java/com/example/ff4JDemoServer/config/SwaggerConfig.java
+++
@@ -1,9 +0,0 @@
-package com.example.ff4JDemoServer.config;
-
-import org.ff4j.spring.boot.web.api.config.EnableFF4jSwagger;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@EnableFF4jSwagger
-public class SwaggerConfig {
-}
